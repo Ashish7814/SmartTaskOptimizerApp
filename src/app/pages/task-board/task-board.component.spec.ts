4e7dd5a9d5e1598cc6958b60116c2fb24abd8cbb
Ashish7814 <82746434+Ashish7814@users.noreply.github.com>
2026-08-18 13:45:50 +0530

    Update task-board.component.spec.ts

--- src/app/pages/task-board/task-board.component.spec.ts
+++ src/app/pages/task-board/task-board.component.spec.ts
@@ -1,21 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { TaskBoardComponent } from './task-board.component';
+import { TaskService } from '../../core/services/task.service';
 
 describe('TaskBoardComponent', () => {
   let component: TaskBoardComponent;
   let fixture: ComponentFixture<TaskBoardComponent>;
 
+  const taskServiceMock = {
+    getTasks: () =>
+      of({
+        items: [],
+        totalCount: 0,
+        page: 1,
+        pageSize: 100
+      }),
+
+    updateStatus: () => of(void 0)
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
-      imports: [TaskBoardComponent]
-    })
-    .compileComponents();
+      imports: [TaskBoardComponent],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: taskServiceMock
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TaskBoardComponent);
     component = fixture.componentInstance;
+
     await fixture.whenStable();
   });
 
